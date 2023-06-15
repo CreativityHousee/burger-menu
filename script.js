5778d0ee8c1b1bd53a4c97bf9b1128fecacd97be
CreativityHousee <creativityhousechannel@gmail.com>
2023-06-15 23:48:15 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const openMenuButton = document.querySelector('#open-menu')
+const burgerMenu = document.querySelector('#menu')
+const menuItems = document.querySelectorAll('li')
+
+openMenuButton.addEventListener('click', e => {
+    (e.target.className == "fa fa-navicon") ? e.target.className = "fa fa-close" : e.target.className = "fa fa-navicon"
+    burgerMenu.classList.toggle('active')
+})
+
+menuItems.forEach(element => {
+    element.addEventListener('click' , () => {
+        setTimeout(()=> {
+            burgerMenu.classList.remove('active')
+            openMenuButton.className = "fa fa-navicon"
+        },200)
+    })
+})
